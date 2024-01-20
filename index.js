de2c126535e1d0c9896e5fb6a2c1fcff664dfb32
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-01-19 23:03:43 -0500

    add validation for color keyword to questions, import modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const color = require('color-name');
 const inquirer = require('inquirer');
-const colorName = require('color-name');
+// const colorName = require('color-name');
 
 const { Text, Square, Circle, Triangle } = require('./lib/shapes');
 const questions = require('./lib/questions')
@@ -10,3 +11,6 @@ const squareInstance = new Square();
 const circleInstance = new Circle();
 const triangleInstance = new Triangle();
 
+inquirer.prompt(questions).then(function(answer){
+    console.log(answer);
+});
