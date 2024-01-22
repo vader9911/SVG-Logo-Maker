e7486d55df62eee5c8c00056a1e0ae4fd759340b
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-01-22 17:57:47 -0500

    add questions for MVP, removed unimplemmented questions/values

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const inquirer = require('inquirer');
 const { Text, Square, Circle, Triangle } = require('./lib/shapes');
 const questions = require('./lib/questions')
 
-const textInstance = new Text();
-const squareInstance = new Square();
-const circleInstance = new Circle();
-const triangleInstance = new Triangle();
+
 
 inquirer.prompt(questions).then(function(answer){
     console.log(answer);
