9d3a1b67630e42d1d3d6e6fc7226da254a7c1f1f
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-01-22 20:18:22 -0500

    add output code to index.js & test output files

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const fs = require('fs');
 const color = require('color-name');
 const inquirer = require('inquirer');
-// const colorName = require('color-name');
 
-const { Text, Square, Circle, Triangle } = require('./lib/shapes');
-const questions = require('./lib/questions')
+// const colorName = require('color-name');
+const saveFile = require('./lib/save');
+const {Shape, Text, Square, Circle, Triangle } = require('./lib/shapes');
+const questions = require('./lib/questions');
+const { text } = require('express');
 
 
 
 inquirer.prompt(questions).then(function(answer){
     console.log(answer);
+
+    
+
+    const textContent = new Text();
+    textContent.setColor(answer.txtColor);
+    textContent.setText(answer.text);
+    
+    let shapeType;
+    switch (answer.shape) {
+         case 'Square':
+            shapeType = new Square();
+            shapeType.setColor(answer.shapeColor);
+            break;
+        case 'Circle':
+            shapeType = new Circle();
+            shapeType.setColor(answer.shapeColor);
+            break;
+        case 'Triangle':
+            shapeType = new Triangle();
+            textContent.y = "70%";
+            shapeType.setColor(answer.shapeColor);
+            break;
+    }
+
+
+    const svg = shapeType.render() + textContent.render();
+
+    const shape = new Shape(svg);
+    
+    
+    
+    saveFile(answer.filename, shape.render());
+
+
 });
+
+
+
+// save(answer.filename)
